d8e5cda6736e2cff49f4d9e3fefd48640d37451d
Danny Martini <danny@wunderflats.com>
2018-03-10 15:07:38 +0100

    --wip--

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -6,22 +6,61 @@ using UnityEngine;
 public class GameMaster : MonoBehaviour {
 
 	public GameObject octupus;
-	public GameObject spawnPoints;
+	public GameObject enemy;
 
-	public GameObject[] enemies;
-
-	GameObject[] spawns;
+	private ArrayList enemies;
+	public Transform[] spawns;
 
 	// Use this for initialization
 	void Start () {
+		enemies = new ArrayList();
 
-
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		foreach (GameObject enemy in enemies) {
+			enemy.transform.position += Vector3.up * 0.1f;
+		}
+
+		if (Input.GetKeyDown("a")) {
+			spawnEnemy(0);
+		}
+
+		if (Input.GetKeyDown("s")) {
+			spawnEnemy(1);
+		}
+
+		if (Input.GetKeyDown("d")) {
+			spawnEnemy(2);
+		}
+
+		if (Input.GetKeyDown("f")) {
+			spawnEnemy(3);
+		}
+
+		if (Input.GetKeyDown("g")) {
+			spawnEnemy(4);
+		}
+
+		if (Input.GetKeyDown("h")) {
+			spawnEnemy(5);
+		}
+
+		if (Input.GetKeyDown("j")) {
+			spawnEnemy(6);
+		}
+
+		if (Input.GetKeyDown("k")) {
+			spawnEnemy(7);
+		}
 	}
 
+	private void spawnEnemy(int lane) {
+		enemies.Add(Instantiate (enemy, spawns[lane].transform.position, Quaternion.identity));
+	}
+
+	private int getRandomLand() {
+		return Random.Range (0, 8);
+	}
 }
