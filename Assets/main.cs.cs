213b7a5120ee5066c4125828a77e5a36cb658e48
Danny Martini <danny@wunderflats.com>
2018-03-10 13:48:05 +0100

    --wip--

--- Assets/main.cs.cs
+++ Assets/main.cs.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class main.cs : MonoBehaviour {
+public class main : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
