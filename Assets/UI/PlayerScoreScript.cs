16cef34b889ee8056cb9cebe14be67665307632e
Niaz <tigrexspalterlp@gmail.com>
2018-03-10 15:04:51 +0100

    -UI

---
+++ Assets/UI/PlayerScoreScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreScript : MonoBehaviour {
+
+    [SerializeField]
+    Text text;
+
+    [SerializeField]
+    GameMaster gameMaster;
+
+    // Use this for initialization
+    void Start () {
+        text.text = "Player Score: " + gameMaster.getScore();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "Player Score: " + gameMaster.getScore();
+    }
+}
---
+++ Assets/UI/StartOnClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartOnClick : MonoBehaviour {
+
+    public Button yourButton;
+    public Image titleScreen;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(load);
+    }
+
+    
+
+    public void load()
+    {
+        titleScreen.enabled = false;
+    }
+}
