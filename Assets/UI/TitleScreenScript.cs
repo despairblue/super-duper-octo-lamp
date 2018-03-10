67b667887b960ebc9205285fd1a04878bb97ece4
Niaz <tigrexspalterlp@gmail.com>
2018-03-10 21:55:23 +0100

    - part two

---
+++ Assets/UI/TitleScreenScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenScript : MonoBehaviour {
+
+    public GameObject TitleScreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (TitleScreen.activeSelf == true) {
+            Time.timeScale = 0f;
+        }
+        
+    }
+}
